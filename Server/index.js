c007377d95ad0a5e58be23c3c41f9ed3c54fa095
Om <omvkrishna0852@gmail.com>
2023-05-04 00:40:51 +0530

    added new logo

--- Server/index.js
+++ Server/index.js
@@ -9,11 +9,12 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
+app.use(cors());
 
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E!')
@@ -23,7 +24,7 @@ const startServer = async () => {
 
     try {
         connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+        app.listen(8070, () => console.log('Server has started on port http://localhost:8080'))
     } catch (error) {
         console.log(error);
     }
