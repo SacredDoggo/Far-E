4aba2039c1a6396b9e2820def90487c24183c09e
Om <omvkrishna0852@gmail.com>
2023-05-08 22:29:16 +0530

    cors shift

--- server/index.js
+++ server/index.js
@@ -9,12 +9,12 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
-app.use(cors());
 
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E!')
